04978f158980cf369fb7161c02cec76fe5fe5952
dervalinor <dervalinor@protonmail.com>
2023-03-21 12:33:31 -0500

    How to create a repository Github CLI and first program solidity

---
+++ 1_first_contract/smart_contract.sol
@@ -0,0 +1,31 @@
+//install solidity linux:sudo apt-get update,sudo apt-get install snapd,
+//sudo snap install solc
+
+//create a new repository: gh repo create [<name>] [flags] 
+//gh repo create Solidity_programming --public
+
+/* 
+This program defines a SimpleStorage contract that has two functions: set and get. The set function takes an unsigned integer (uint) as an argument and assigns its value to the storedData state variable. The get function returns the current value of storedData.
+*/
+
+// This is a comment. Comments are used to explain what the code does.
+
+// This line specifies the version of Solidity that the code is written in.
+pragma solidity ^0.8.0;
+
+// This is a contract. A contract is a collection of code and data that resides at a specific address on the Ethereum blockchain.
+contract SimpleStorage {
+    // This is a state variable. State variables are permanently stored in contract storage.
+    uint storedData;
+
+    // This is a function. Functions define the behavior of the contract.
+    function set(uint x) public {
+        // This line assigns the value of 'x' to the 'storedData' state variable.
+        storedData = x;
+    }
+
+    // This function returns the value of 'storedData'.
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Learn Solidity and GitHub CLI
+
+Welcome to our repository where you can learn how to program in Solidity and use the GitHub CLI!
+
+## What is Solidity?
+
+Solidity is a contract-oriented programming language that is used to write smart contracts on the Ethereum blockchain.
+
+## What is the GitHub CLI?
+
+The GitHub CLI is a command-line tool that allows you to interact with GitHub from your terminal.
+
+## Getting Started
+
+To get started with this repository, you'll need to have some basic knowledge of programming and be familiar with using the command line. You'll also need to have [Node.js](https://nodejs.org/) installed on your computer.
+
+1. Clone this repository by running `git clone https://github.com/your-username/your-repository-name.git`.
+2. Navigate into the repository by running `cd your-repository-name`.
+3. Install dependencies by running `npm install`.
+4. Start learning by following the tutorials in the `tutorials` directory.
+
+## Contributing
+
+We welcome contributions from anyone who wants to help improve this repository! If you'd like to contribute, please fork this repository and create a pull request with your changes.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
